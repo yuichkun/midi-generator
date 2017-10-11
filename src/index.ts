542c9c12dbe46720e1e1d6c191e6728868b59c11
Yuichi Yamamoto <Yuichkun@gmail.com>
2017-10-11 21:34:06 +0900

    add sigint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
+import { Logger } from './Logger';
 import { MidiGenerator, INote } from './MidiGenerator';
 
-console.log("start app");
+process.on('SIGINT', function(){
+  Logger.system("EXIT: SongGenerator");
+  MidiGenerator.flush();
+  process.exit();
+});
+
+Logger.system("App started");
 const note:INote = {
   port: 0,
   channel: 0,
   pitch: 60,
-  length: 500  
+  length: 50  
 };
+
+setInterval(()=>{
 MidiGenerator.noteOut(note);
+}, 100);
